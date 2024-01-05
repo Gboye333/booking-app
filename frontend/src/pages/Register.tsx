6a7fdfa48424b2aac8ed3bbeaf5eff854d3d0083
Gboyega <gboyes333@gmail.com>
2024-01-05 23:05:37 +0100

    new commit

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -12,16 +12,58 @@ const Register = () => {
   return (
     <form className="flex flex-col gap-5">
       <h2 className="text-3xl font-bold">Create an account</h2>
-      <div className="flex flex-col md:flex:row gap-5">
-        <label className="text-gray-700 text-sm font-bold">
+      <div className="flex flex-col md:flex-row gap-5">
+        <label className="text-gray-700 text-sm font-bold flex-1">
           First Name
-          <input className="border rounded w-full py-1 px-2 font-normal"></input>
+          <input
+            className="border rounded w-full py-1 px-2 font-normal"
+            {...register("firstName", { required: "This field is required" })}
+          ></input>
         </label>
-        <label className="text-gray-700 text-sm font-bold">
+        <label className="text-gray-700 text-sm font-bold flex-1">
           Second Name
-          <input className="border rounded w-full py-1 px-2 font-normal"></input>
+          <input
+            className="border rounded w-full py-1 px-2 font-normal"
+            {...register("lastName", { required: "This field is required" })}
+          ></input>
         </label>
       </div>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Email
+        <input
+          type="email"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("email", { required: "This field is required" })}
+        ></input>
+      </label>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Password
+        <input
+          type="password"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("password", {
+            required: "This field is required",
+            minLength: { 
+              value: 6,
+              message: "password must be at least 6 characters"
+             },
+          })}
+        ></input>
+      </label>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Confirm Password
+        <input
+          type="password"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("confirmPassword", {
+           validate:(val)=>{
+            if(!val){
+              return "This field is required";
+            }
+           } 
+          })}
+        ></input>
+      </label>
     </form>
   );
 };
