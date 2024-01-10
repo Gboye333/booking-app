0db14839a4c559170a5ebf17522fffab1c4d56b2
Gboyega <gboyes333@gmail.com>
2024-01-10 01:02:04 +0100

    my new commit

--- frontend/src/contexts/AppContext.tsx
+++ frontend/src/contexts/AppContext.tsx
@@ -1,15 +1,41 @@
-import React from "react"
+import React, { createContext, useContext, useState } from "react";
+import Toast from "../components/Toast";
+
 type ToastMessage = {
-    message: string,
-    type: "SUCCESS" | "ERROR";
-}
+  message: string;
+  type: "SUCCESS" | "ERROR";
+};
 
-type AppContext = {
-   showToast: (toastMessage: ToastMessage)=> void
-}
+type AppContextType = {
+  showToast: (toastMessage: ToastMessage) => void;
+};
 
-const AppContext = React.createContext<AppContext | undefined>(undefined)
+const AppContext = createContext<AppContextType | undefined>(undefined);
 
-export const AppContextProvider = ({children}: { children: React.ReactNode}) => {
+export const AppContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+    const  [toast, setToast] = useState<ToastMessage | undefined>(undefined)
+  return (
+    <AppContext.Provider
+      value={{
+        showToast: (toastMessage) => {
+          setToast(toastMessage);
+        },
+      }}
+    >
+      {toast && (<Toast message={toast.message} type ={toast.type} onClose={()=> setToast(undefined)}/>)}
+      {children}
+    </AppContext.Provider>
+  );
+};
 
-}
+export const useAppContext = () => {
+  const context = useContext(AppContext);
+  if (!context) {
+    throw new Error("useAppContext must be used within AppContextProvider");
+  }
+  return context;
+};
